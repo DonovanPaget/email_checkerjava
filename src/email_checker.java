3e3b85a57905a0470446aa57f4e434a5df24d77f
Donovan Paget <donovan.p56@gmail.com>
2019-11-14 16:45:41 -0700

    Initial commit

---
+++ src/email_checker.java
@@ -0,0 +1,16 @@
+public class email_checker {
+    public static void main(String[] args){
+        System.out.println("Hello");
+
+        // open emails csv file
+
+        // process csv file as lines
+        // check if in declines
+
+        // suggest fix
+
+        // organize
+
+        // print to output files
+    }
+}
